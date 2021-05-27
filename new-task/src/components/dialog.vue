17692f098bfa659dce779ce5a68af9ed4db9b1bb
sargis2121 <saq2121y@gmail.com>
2021-05-27 07:06:47 -0800

    task with vuetify

--- new-task/src/components/dialog.vue
+++ new-task/src/components/dialog.vue
@@ -1,17 +1,8 @@
 <template>
-      <v-row justify="center">
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="600px"
-    >
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="600px">
       <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          color="primary"
-          dark
-          v-bind="attrs"
-          v-on="on"
-        >
+        <v-btn color="primary" dark v-bind="attrs" v-on="on">
           Open Dialog
         </v-btn>
       </template>
@@ -22,32 +13,22 @@
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
+              <v-col cols="12" sm="6" md="4">
                 <v-text-field
+                  v-model="firstName"
                   label="Legal first name*"
                   required
                 ></v-text-field>
               </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
+              <v-col cols="12" sm="6" md="4">
                 <v-text-field
                   label="Legal middle name"
                   hint="example of helper text only on focus"
                 ></v-text-field>
               </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-                md="4"
-              >
+              <v-col cols="12" sm="6" md="4">
                 <v-text-field
+                  v-model="lastName"
                   label="Legal last name*"
                   hint="example of persistent helper text"
                   persistent-hint
@@ -56,6 +37,7 @@
               </v-col>
               <v-col cols="12">
                 <v-text-field
+                  v-model="userEmail"
                   label="Email*"
                   required
                 ></v-text-field>
@@ -67,22 +49,27 @@
                   required
                 ></v-text-field>
               </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
+              <v-col cols="12" sm="6">
                 <v-select
+                  v-model="userAge"
                   :items="['0-17', '18-29', '30-54', '54+']"
                   label="Age*"
                   required
                 ></v-select>
               </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
+              <v-col cols="12" sm="6">
                 <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  :items="[
+                    'Skiing',
+                    'Ice hockey',
+                    'Soccer',
+                    'Basketball',
+                    'Hockey',
+                    'Reading',
+                    'Writing',
+                    'Coding',
+                    'Basejump'
+                  ]"
                   label="Interests"
                   multiple
                 ></v-autocomplete>
@@ -93,18 +80,10 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
+          <v-btn color="blue darken-1" text @click="dialog = false">
             Close
           </v-btn>
-          <v-btn
-            color="blue darken-1"
-            text
-            @click="dialog = false"
-          >
+          <v-btn color="blue darken-1" text @click="saveChange">
             Save
           </v-btn>
         </v-card-actions>
@@ -114,9 +93,33 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      dialog: false,
-    }),
+import { mapGetters, mapMutations } from 'vuex'
+export default {
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      userEmail: '',
+      userAge: '',
+      dialog: false
+    }
+  },
+  computed: mapGetters(['userList']),
+  methods: {
+    ...mapMutations(['addUser']),
+
+    saveChange () {
+    //   debugger
+      if (this.firstName && this.lastName && this.userEmail && this.userAge) {
+        const user = {
+          fullname: this.firstName + this.lastName,
+          age: this.userAge,
+          email: this.userEmail
+        }
+        this.addUser(user)
+        this.dialog = false
+      }
+    }
   }
+}
 </script>
--- new-task/src/store/index.js
+++ new-task/src/store/index.js
@@ -5,11 +5,32 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: [
+      {
+        fullname: 'Frozen Yogurt',
+        age: '159',
+        email: 'aasas'
+      },
+      {
+        fullname: 'Ice cream sandwich',
+        age: '237',
+        email: 'aasas'
+      }
+    ]
   },
   mutations: {
+    addUser (state, list) {
+      state.user.push(list)
+    }
   },
   actions: {
+    // addList ({ commit }, list) {
+    //   commit('addUser', list)
+    // }
   },
-  modules: {
+  getters: {
+    userList (state) {
+      return state.user
+    }
   }
 })
